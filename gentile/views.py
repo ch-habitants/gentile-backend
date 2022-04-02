9590bde3cc386515dcd728a7bd769fd95d453648
Pierre Bürki <pierre.burki@he-arc.ch>
2022-04-02 15:35:12 +0200

    Create views and urls.

--- gentile/views.py
+++ gentile/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from gentile.models import Commune
+from gentile.serializers import CommuneSerializer
+
+
+class CustomViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet
+):
+    """
+    Custom viewset allowing only for models to be listed and retrieved.
+    """
+    ...
+
+
+class CommuneViewSet(CustomViewSet):
+    """
+    API endpoint to get all the communes and their gentilés.
+    """
+    queryset = Commune.objects.all().order_by('name_fr')
+    serializer_class = CommuneSerializer
