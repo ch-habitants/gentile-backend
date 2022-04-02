203af86917486770a58032c8684e747f6ff2760a
Pierre Bürki <pierre.burki@he-arc.ch>
2022-04-02 15:35:12 +0200

    Create views and urls.

--- gentile_backend/urls.py
+++ gentile_backend/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from gentile import views
+
+router = routers.DefaultRouter()
+router.register(r'communes', views.CommuneViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
