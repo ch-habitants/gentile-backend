0a3341a41c9009ad90d811187e945f152130ecdf
Pierre Bürki <pierre.burki@he-arc.ch>
2022-04-08 12:48:31 +0200

    Fill search-friendly fields on save.

--- gentile/models/commune.py
+++ gentile/models/commune.py
@@ -1,6 +1,9 @@
 from gentile.models.canton import Canton
 from django.db import models
 
+from typing import Any
+from unidecode import unidecode
+
 
 class Commune(models.Model):
 
@@ -11,3 +14,18 @@ class Commune(models.Model):
 
     searchable_name = models.CharField(max_length=100, blank=True)
     searchable_gentile = models.CharField(max_length=150)
+
+    def save(self, *args: Any, **kwargs: Any) -> None:
+        def generate_searchable_string(base_string: str) -> str:
+            return (
+                unidecode(base_string)
+                .replace('-', ' ')
+                .replace(',', '')
+                .replace('Saint', 'saint st')
+                .lower()
+            )
+
+        self.searchable_name = generate_searchable_string(self.name_fr)
+        self.searchable_gentile = generate_searchable_string(self.gentile_fr)
+
+        super().save(*args, **kwargs)
