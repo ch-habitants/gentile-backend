eb0adb3afc6829292eb0aa3c287efa17760e5e6e
Pierre Bürki <pierre.burki@he-arc.ch>
2022-04-30 16:15:42 +0200

    Workaround for overriden Commune.save not being called in seeder.

--- gentile/models/commune.py
+++ gentile/models/commune.py
@@ -5,6 +5,16 @@ from typing import Any
 from unidecode import unidecode
 
 
+def generate_searchable_string(base_string: str) -> str:
+    return (
+        unidecode(base_string)
+        .replace('-', ' ')
+        .replace(',', '')
+        .replace('Saint', 'saint st')
+        .lower()
+    )
+
+
 class Commune(models.Model):
 
     name_fr = models.CharField(max_length=50)
@@ -16,15 +26,6 @@ class Commune(models.Model):
     searchable_gentile = models.CharField(max_length=150)
 
     def save(self, *args: Any, **kwargs: Any) -> None:
-        def generate_searchable_string(base_string: str) -> str:
-            return (
-                unidecode(base_string)
-                .replace('-', ' ')
-                .replace(',', '')
-                .replace('Saint', 'saint st')
-                .lower()
-            )
-
         self.searchable_name = generate_searchable_string(self.name_fr)
         self.searchable_gentile = generate_searchable_string(self.gentile_fr)
 
